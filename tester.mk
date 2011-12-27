ccceb4ad1244bd0c6c83ff6569196e8e38a05fe4
Magnus Ernstsson <magnus.ernstsson@patchworksolutions.com>
2011-12-27 12:07:12 +0100

    Added support for multiple test cases with code coverage report

--- tester.mk
+++ tester.mk
@@ -1,18 +1,33 @@
 TESTEROUT := out/int/tester
-TESTERCLARITYLIB := $(TESTEROUT)/libclaritycore.a
-TESTERCLARITYINCLUDE := $(TESTEROUT)/include/
+TESTERLIB := $(TESTEROUT)/libclaritycore.a
+TESTERINCLUDE := $(TESTEROUT)/include/
 TESTERCLARITYTEST := $(TESTEROUT)/claritytest
-TESTERREPORT := $(TESTEROUT)/report
-TESTERSOURCE := $(wildcard test/core/*.c)
+TESTERSOURCE := $(wildcard test/*/*.c)
+TESTERDADIR := out/int/arch/x86/tester/coverage/src
+TESTERTESTS := \
+	$(addprefix $(TESTEROUT)/, \
+	$(patsubst %.c,%.test, $(TESTERSOURCE)))
+TESTERTRACES := \
+	$(addprefix $(TESTEROUT)/, \
+	$(patsubst %.c,%.info, $(TESTERSOURCE)))
+TESTERCOVERAGE := out/int/tester/coverage
 
-.PHONY : clean
+$(TESTERCOVERAGE) : $(TESTERCOVERAGE).info
+	genhtml --num-spaces 4 -c test/gcov.css -q -s -b $(TESTERCOVERAGE)init.info -t "Clarity" -o $@ $@.info
 
-$(TESTERREPORT) : $(TESTERCLARITYTEST)
-	$(TESTERCLARITYTEST)
-	@ touch $(TESTERREPORT)	
+$(TESTERCOVERAGE).info : $(TESTERTRACES) $(TESTERCOVERAGE)init.info
+	lcov -q -a $(TESTERCOVERAGE)init.info $(addprefix -a , $(TESTERTRACES)) -o $@
 
-$(TESTERCLARITYTEST) : $(TESTERCLARITYLIB)
-	gcc $(abspath $(TESTERSOURCE)) $< -I$(TESTERCLARITYINCLUDE) -lgcov -o $@
+$(TESTERCOVERAGE)init.info : $(TESTERTESTS)
+	lcov -q -c -i -d $(TESTERDADIR) -o $@
 
-$(TESTERCLARITYLIB) :
-	@ $(MAKE) -f target.mk ARCH=x86 MACH=default TARGET=coverage OUT=$(TESTEROUT)
+$(TESTEROUT)/%.info : $(TESTEROUT)/%.test $(TESTERCOVERAGE)init.info
+	valgrind --error-exitcode=1 --leak-check=yes -q --dsymutil=yes $<
+	lcov -q -c -t $(notdir $(basename $@)) -d $(TESTERDADIR) -o $@
+
+$(TESTEROUT)/%.test : %.c $(TESTERLIB)
+	@ mkdir -p $(dir $@)
+	gcc $< $(TESTERLIB) -I$(TESTERINCLUDE) -lgcov -o $@
+
+$(TESTERLIB) :
+	@ $(MAKE) -f target.mk ARCH=x86 MACH=tester TARGET=coverage OUT=$(TESTEROUT)
