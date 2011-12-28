db2867c7f983f2182ca9f54c947eaa460cfd9aeb
Magnus Ernstsson <magnus.ernstsson@patchworksolutions.com>
2011-12-28 14:39:44 +0100

    Removed break on test error. A test report is no created instead.

--- tester.mk
+++ tester.mk
@@ -13,17 +13,28 @@ TESTERTRACES := \
 TESTERCOVERAGE := out/int/tester/coverage
 
 $(TESTERCOVERAGE) : $(TESTERCOVERAGE).info
-	genhtml --num-spaces 4 -c test/gcov.css -q -s -b $(TESTERCOVERAGE)init.info -t "Clarity" -o $@ $@.info
+	@ mkdir -p out/rel
+	genhtml --num-spaces 4 -q -c test/gcov.css -b $(TESTERCOVERAGE)init.info -t "Clarity" -o $@ $@.info
+	@ cat $(TESTEROUT)/testreport.txt > out/rel/testreport.txt
 
 $(TESTERCOVERAGE).info : $(TESTERTRACES) $(TESTERCOVERAGE)init.info
+	@ echo "\nCoverage:" >> $(TESTEROUT)/testreport.txt
 	lcov -q -a $(TESTERCOVERAGE)init.info $(addprefix -a , $(TESTERTRACES)) -o $@
+	lcov -l $@  >> $(TESTEROUT)/testreport.txt
 
 $(TESTERCOVERAGE)init.info : $(TESTERTESTS)
+	@ echo Test Cases: > $(TESTEROUT)/testreport.txt
 	lcov -q -c -i -d $(TESTERDADIR) -o $@
 
 $(TESTEROUT)/%.info : $(TESTEROUT)/%.test $(TESTERCOVERAGE)init.info
-	valgrind --error-exitcode=1 --leak-check=yes -q --dsymutil=yes $<
+	-valgrind --error-exitcode=1 --leak-check=yes -q --dsymutil=yes $< ;\
+	if [ $$? -eq 0 ] ; then \
+		echo $(notdir $(basename $@)) Passed >> $(TESTEROUT)/testreport.txt; \
+	else \
+		echo $(notdir $(basename $@)) Failed >> $(TESTEROUT)/testreport.txt; \
+	fi ;
 	lcov -q -c -t $(notdir $(basename $@)) -d $(TESTERDADIR) -o $@
+	
 
 $(TESTEROUT)/%.test : %.c $(TESTERLIB)
 	@ mkdir -p $(dir $@)
