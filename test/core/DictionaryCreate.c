c79e047481f199db27b6139f2fc40a421124b1c9
Magnus Ernstsson <magnus.ernstsson@patchworksolutions.com>
2011-12-29 14:46:00 +0100

    Added dictionary constructor test

---
+++ test/core/DictionaryCreate.c
@@ -0,0 +1,45 @@
+#include "Clarity.h"
+#include "ClarityHeap.h"
+#include "ClarityMacro.h"
+#include "ClarityDictionary.h"
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+static void *mainAlloc(Uint32 size)
+{
+	return malloc(size);
+}
+
+static void mainFree(void *data)
+{
+	free(data);
+}
+
+static Sint8 compare(Clarity *clarity, void *first, void *second)
+{
+	UNUSED(clarity);
+	UNUSED(first);
+	UNUSED(second);
+}
+
+static void entry(Clarity *clarity, void *data)
+{
+	ClarityDictionary *dictionary;
+
+	UNUSED(data);
+	dictionary = clarityDictionaryCreate(clarity, compare);
+	assert(dictionary != NULL);
+}
+
+int main(void)
+{
+	ClarityHeap *heap;
+	Clarity *clarity;
+
+	heap = clarityHeapCreateExternal(mainAlloc, mainFree);
+	clarity = clarityCreate(entry, heap);
+	clarityStart(clarity);
+	clarityStop(clarity);
+	return 0;
+}
