d141a3cdc28ae1329846dc73b6a2de8160689fdd
Magnus Ernstsson <magnus.ernstsson@patchworksolutions.com>
2011-12-29 20:56:46 +0100

    Added test for array constructor.

---
+++ test/core/ArrayCreate.c
@@ -0,0 +1,39 @@
+#include "Clarity.h"
+#include "ClarityHeap.h"
+#include "ClarityMacro.h"
+#include "ClarityArray.h"
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+static void *mainAlloc(Uint32 size)
+{
+	return malloc(size);
+}
+
+static void mainFree(void *data)
+{
+	free(data);
+}
+
+static void entry(Clarity *clarity, void *data)
+{
+	ClarityArray *array;
+
+	UNUSED(data);
+	array = clarityArrayCreate(clarity);
+
+	assert(array != NULL);
+}
+
+int main(void)
+{
+	ClarityHeap *heap;
+	Clarity *clarity;
+
+	heap = clarityHeapCreateExternal(mainAlloc, mainFree);
+	clarity = clarityCreate(entry, heap);
+	clarityStart(clarity);
+	clarityStop(clarity);
+	return 0;
+}
