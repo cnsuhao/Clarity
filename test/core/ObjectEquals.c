bbb44202370ed0c485cba0fbb99bf78059c0b032
Magnus Ernstsson <magnus.ernstsson@patchworksolutions.com>
2012-01-09 13:56:58 +0900

    Added test of object equals.

---
+++ test/core/ObjectEquals.c
@@ -0,0 +1,53 @@
+#include "Clarity.h"
+#include "ClarityHeap.h"
+#include "ClarityStringObject.h"
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+static void *mainAlloc(Uint32 size)
+{
+	return malloc(size);
+}
+
+static void mainFree(void *data)
+{
+	free(data);
+}
+
+static void entry(ClarityCore *core)
+{
+	Bool equals;
+	ClarityObject *parameters;
+	ClarityObject *object1;
+	ClarityObject *object2;
+
+	object1 = clarityObjectCreate(core);
+	object2 = clarityObjectCreate(core);
+	parameters = clarityObjectCreate(core);
+	clarityObjectSetMember(parameters, "$0", object1);
+	clarityObjectSetMember(parameters, "$1", object1);
+	equals = clarityBooleanGetValue((ClarityBoolean *)clarityObjectGetInnerData(
+		clarityFunctionObjectCall(
+		clarityObjectGetMember(object1, "equals"), parameters)));
+	assert(equals);
+	parameters = clarityObjectCreate(core);
+	clarityObjectSetMember(parameters, "$0", object1);
+	clarityObjectSetMember(parameters, "$1", object2);
+	equals = clarityBooleanGetValue((ClarityBoolean *)clarityObjectGetInnerData(
+		clarityFunctionObjectCall(
+		clarityObjectGetMember(object1, "equals"), parameters)));
+	assert(!equals);
+}
+
+int main(void)
+{
+	ClarityHeap *heap;
+	ClarityCore *core;
+
+	heap = clarityHeapCreateExternal(mainAlloc, mainFree);
+	core = clarityCreate((ClarityEvent)entry, heap);
+	clarityStart(core);
+	clarityStop(core);
+	return 0;
+}
